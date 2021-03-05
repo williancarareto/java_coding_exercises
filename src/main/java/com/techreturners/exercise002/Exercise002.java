5033282cec2db4bf893efa3f4110ac6dda82a7d0
willian <williancarareto@gmail.com>
2021-03-05 17:56:51 +0000

    Exercise002

--- src/main/java/com/techreturners/exercise002/Exercise002.java
+++ src/main/java/com/techreturners/exercise002/Exercise002.java
@@ -1,21 +1,15 @@
 package com.techreturners.exercise002;
-import static org.junit.Assert.assertEquals;
-import org.junit.Test;
 
 public class Exercise002 {
 
     public boolean isFromManchester(Person person){
         // Add your code here
-        if(person.getCity().equals("Manchester")){
-            return true;
-        } 
-            return false;
+        return person.getCity().equals("Manchester"); 
     }     
+
     public boolean canWatchFilm(Person person, int ageLimit) {
         // Add your code here
-        if(person.getAge()){
-            return true;
-        }
-        return false;
+        return person.getAge()>=ageLimit;
     }  
 }
+
